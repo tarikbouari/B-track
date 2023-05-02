0c33c85297ece32f7c76ab61482b62edf874b0a6
tarikbouari <tarikbouari@gmail.com>
2023-05-02 22:58:02 +0000

    Table-update

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,3 +1,34 @@
 class EntitiesController < ApplicationController
-  def index; end
+  before_action :set_entity, only: %i[show edit update destroy]
+  
+  def index
+    @entities = Entity.find(params[:id])
+  end
+
+  def new
+    @groups = Group.find(params[:id])
+    @new_entity = Entity.new
+    
+    @expense = @entity.expenses.build
+  end
+  def create
+    @entity = Entity.new(entity_params)
+    respond_to do |format|
+      if @entity.save
+        format.html { redirect_to group_path(@entity.expenses[0].group_id), notice: 'Entity was successfully created.' }
+        format.json { render :show, status: :created, location: @entity }
+      end
+    end
+  end
+
+  private
+
+   # Use callbacks to share common setup or constraints between actions.
+   def set_entity
+    @entity = Entity.find(params[:id])
+  end
+
+  def entity_params
+    params.require(:entity).permit(:name, :icon, :amount, expenses_attributes: [:group_id])
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,41 @@
 class GroupsController < ApplicationController
+  before_action :current_group, only: %i[show edit update destroy]
   def index
     @groups = Group.all
   end
+
+  def show 
+    @group = Group.find(params[:id])
+    @entities = @group.entities.order('created_at DESC')
+  end
+
+  def new 
+    @new_group = Group.new
+  end
+
+
+ # create a new group in the html and json format
+  def create
+    @group = Group.new(group_parms)
+    respond_to  do |format|
+      if @group.save
+        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        format.json { render :show, status: :created, location: @group}
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+   def group_parms
+    params.require(:group).permit(:name, :icon, :user_id)
+  end
+
+  def current_group
+    @group = Group.find(params[:id])
+  end
+
 end
