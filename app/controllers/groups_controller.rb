f1e1ef61a38f1faa9b63402a1c27edb4dc87485f
tarikbouari <tarikbouari@gmail.com>
2023-05-03 15:10:49 +0000

    Update group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,11 @@
 class GroupsController < ApplicationController
   before_action :current_group, only: %i[show edit update destroy]
   def index
-    @groups = Group.all
+    @groups = Group.all.where(user_id: current_user.id)
   end
 
   def show 
-    @group = Group.find(params[:id])
-    @entities = @group.entities.order('created_at DESC')
+   
   end
 
   def new 
@@ -19,7 +18,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_parms)
     respond_to  do |format|
       if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group}
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -31,7 +30,11 @@ class GroupsController < ApplicationController
   private
 
    def group_parms
-    params.require(:group).permit(:name, :icon, :user_id)
+    params.require(:group).permit(:name, :icon).merge(user_id: current_user.id)
+  end
+  
+  def set_author
+    @author = current_user
   end
 
   def current_group
